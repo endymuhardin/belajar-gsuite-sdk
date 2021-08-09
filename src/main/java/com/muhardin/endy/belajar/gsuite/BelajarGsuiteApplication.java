7ec72db740052ad64dd513c102c69053f84631e6
Endy Muhardin <endy.muhardin@gmail.com>
2021-08-09 15:02:48 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/gsuite/BelajarGsuiteApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.gsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarGsuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarGsuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhardin/endy/belajar/gsuite/BelajarGsuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.gsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarGsuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
